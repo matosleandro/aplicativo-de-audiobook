3e23c8b0c7faf8778509276c8c97205b208262be
Leandro Matos <leandro.matos@live.com>
2024-01-30 00:40:56 -0300

    fix: correção na função de dar play e pause

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,28 @@
-const audioCapitulo = document.getElementById('audio-capitulo');
 const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
 let taTocando = 0;
 
 function tocarFaixa() {
     audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-fill');
+    botaoPlayPause.classList.add('bi-pause-fill');
 }
 function pausarFaixa() {
     audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-fill');
+    botaoPlayPause.classList.add('bi-play-fill');
 }
 // Função para tocar ou pausar a faixa de audio
 function tocarOuPausar() {
     if ( taTocando === 0) {
         tocarFaixa();
+        taTocando = 1;
     } else {
         pausarFaixa();
+        taTocando = 0;
     }
 }
 
-botaoPlayPause.addEventListener('click', tocarFaixa);
+botaoPlayPause.addEventListener('click', tocarOuPausar);
