41b1adee62d2dd4379f0e8f6b173bea746638804
Leandro Matos <leandro.matos@live.com>
2024-01-30 00:21:07 -0300

    add: function para tocar ou pausar a faixa

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,21 @@ const audioCapitulo = document.getElementById('audio-capitulo');
 const botaoPlayPause = document.getElementById('play-pause');
 
 const numeroCapitulos = 10;
+let taTocando = 0;
 
 function tocarFaixa() {
     audioCapitulo.play();
+}
+function pausarFaixa() {
+    audioCapitulo.pause();
+}
+// Função para tocar ou pausar a faixa de audio
+function tocarOuPausar() {
+    if ( taTocando === 0) {
+        tocarFaixa();
+    } else {
+        pausarFaixa();
+    }
 }
 
 botaoPlayPause.addEventListener('click', tocarFaixa);
